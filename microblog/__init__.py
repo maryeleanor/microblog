4fa43f9436878710caffdbab16af0f8b83ac6539
maryeleanor <marycasserly@gmail.com>
2020-03-23 12:12:25 -0700

    first draft of microblog

---
+++ microblog/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager    
+from flask_mail import Mail
+
+app = Flask(__name__)
+
+# secret key for users to stay logged in
+app.config['SECRET_KEY'] = '348943509rf4j09w09809cv987v987b'
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# config database with sqlalchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ideas.db'
+db = SQLAlchemy(app)
+ 
+#confit login and pw hass
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+bcrypt = Bcrypt(app)
+
+# config email service for PW reset
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+# noQA
+from microblog import routes
